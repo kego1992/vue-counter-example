55c867a46478889d8f121a6e7115de6ea49da4d5
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-09-13 18:04:29 +0900

    Add counter app

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Vue Counter App</h1>
+        <counter/>
+    </div>
+</template>
+
+<script>
+    import Counter from './Counter'
+
+
+    export default {
+        components: {Counter},
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        text-align: center;
+    }
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button @click="decrement">-</button>
+        <span>{{count}}</span>
+        <button @click="increment">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        increment() {
+            this.count += 1
+        },
+        decrement() {
+            this.count -= 1
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    $button-size: 40px;
+    button {
+        display: inline-block;
+        width: $button-size;
+        height: $button-size / 2;
+        background-color: #666;
+        color: #fff;
+        border: 1px solid #fff;
+        border-radius: 3px;
+    }
+
+    span {
+        font-weight: bold;
+        font-size: $button-size / 2;
+    }
+
+    div {
+        text-align: center;
+        padding: 1em;
+        background-color: #fafafa;
+        border: 1px solid #ccc;
+        border-radius: 3px;
+    }
+</style>
